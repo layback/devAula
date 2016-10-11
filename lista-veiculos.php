3ad21175a664fcc38a7e729371a3e21818f81461
diego <diego20rodrigues@hotmail.com>
2016-10-11 17:01:46 -0300

    lista veiculos

---
+++ lista-veiculos.php
@@ -0,0 +1,60 @@
+<html>
+		<head>
+		<link rel="stylesheet" type="text/css" href="css/estilo.css">
+		<script src="js/validaDados.js"></script>
+		<script src="js/jquery-3.1.1.min.js"></script>
+	</head>
+	<body>		
+		<div class="externa_tela_cheia">
+			<div class="cabecalho_tela_cheia">
+				<h2>VEÍCULOS CADASTRADOS</h2>
+			</div>
+			
+			<?php			
+				$conexao = mysqli_connect('localhost', 'root', '', 'locadoraveiculos');	
+				$veiculos = array();
+				$resultado = mysqli_query($conexao, "select * from veiculo");
+				while($veiculo = mysqli_fetch_assoc($resultado)){
+					array_push($veiculos, $veiculo);
+				}
+			?>
+			
+			<table style="width:100%">
+				<tr>
+					<th>Marca</th>
+					<th>Modelo</th> 
+					<th>Ano</th>
+					<th>Dir.Hid</th>
+					<th>Ar Cond</th>
+					<th>Air Bag</th>
+					<th>Alarme</th>
+					<th>Banco Couro</th>
+					<th>Som</th>
+					<th>Travas</th>
+					<th>Piloto</th>
+					<th>Outro</th>
+				</tr>			
+				<?php
+					foreach($veiculos as $veiculo){
+				?>
+				<tr>
+					<td class="coluna"><?= $veiculo['marca'] ?></td>
+					<td class="coluna"><?= $veiculo['modelo'] ?></td>
+					<td class="coluna"><?= $veiculo['ano'] ?></td>
+					<td class="coluna"><?= $veiculo['direcao'] ?></td>
+					<td class="coluna"><?= $veiculo['ar_condicionado'] ?></td>
+					<td class="coluna"><?= $veiculo['air_bag'] ?></td>
+					<td class="coluna"><?= $veiculo['alarme'] ?></td>
+					<td class="coluna"><?= $veiculo['banco_couro'] ?></td>
+					<td class="coluna"><?= $veiculo['som'] ?></td>
+					<td class="coluna"><?= $veiculo['travas'] ?></td>
+					<td class="coluna"><?= $veiculo['piloto_automatico'] ?></td>
+					<td class="coluna"><?= $veiculo['outro'] ?></td>
+				</tr>
+				<?php
+					}
+				?>
+			</table>
+		</div>
+	</body>
+</html>
